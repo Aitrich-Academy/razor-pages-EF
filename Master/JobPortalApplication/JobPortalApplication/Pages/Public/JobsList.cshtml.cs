30d1ebb727ca698715316d10ec329ab6e3604a35
Anuvind <soudha.aitrich@gmail.com>
2023-07-06 12:12:08 +0530

    UserId From Login

--- Master/JobPortalApplication/JobPortalApplication/Pages/Public/JobsList.cshtml.cs
+++ Master/JobPortalApplication/JobPortalApplication/Pages/Public/JobsList.cshtml.cs
@@ -10,29 +10,29 @@ namespace HireMeNow_JobSeekerApp.Pages.Public
 {
     public class JobsListModel : PageModel
     {
-		private readonly IUserService userService;
+        private readonly IUserService userService;
         private readonly IJobService jobService;
         private readonly IApplicationService applicationService;
         private readonly IMapper _mapper;
 
-		public JobsListModel(IUserService userService, IMapper mapper,IJobService _jobService, IApplicationService _applicationService)
-		{
-			userService = userService;
-			_mapper = mapper;
-            jobService = _jobService;
-            applicationService = _applicationService;   
-		}
-
-		[BindProperty]
-        public List<Job> jobs { get; set; } = new List<Job>();
-        public ApplicationDto input { get; set;}
-       
-
-        public void OnGet()
+        public JobsListModel(IUserService userService, IMapper mapper, IJobService _jobService, IApplicationService _applicationService)
         {
-           var userId= HttpContext.Session.GetInt32("UserId");
-            TempData["UserId"] =userId;
-           jobs= jobService.GetJobs();
+            userService = userService;
+            _mapper = mapper;
+            jobService = _jobService;
+            applicationService = _applicationService;
+        }
+
+        [BindProperty]
+        public List<Job> jobs { get; set; } = new List<Job>();
+        public ApplicationDto input { get; set; }
+        public string userId {get;set;}
+
+		public void OnGet()
+        {
+             userId = HttpContext.Session.GetString("UserId");
+            //TempData["UserId"] =userId;
+            jobs = jobService.GetJobs();
            
 
         }
@@ -45,17 +45,17 @@ namespace HireMeNow_JobSeekerApp.Pages.Public
         public IActionResult OnPostButtonClick()
         {
             var Application = _mapper.Map<Application>(input);
-      
-            Guid userid =new Guid("18467cd4-2b85-4b2e-ab33-c78353d8b36d");
-            //Guid id = (Guid)TempData["Id"];
-           
-            //User user = userService.getById(Guid.new(userid));
-            //TempData.Keep("UserId");
 
-            if (Guid.TryParse(Request.Form["parameter"], out Guid jobid))
+			//////Guid userid =new Guid("18467cd4-2b85-4b2e-ab33-c78353d8b36d");
+			//string userid = (string)TempData["UserId"];
+
+			////User user = userService.getById(new Guid(userid));
+			//TempData.Keep("UserId");
+			userId = HttpContext.Session.GetString("UserId");
+			if (Guid.TryParse(Request.Form["parameter"], out Guid jobid))
             {
 
-                applicationService.AddApplication(jobid, userid);
+                applicationService.AddApplication(jobid, (new Guid(userId)));
                 return RedirectToPage("/JobSeeker/AppliedJobs");
 
             }
